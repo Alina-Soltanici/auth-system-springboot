838fc470cf96ba82eb6e013513714264c9564db0
alinalinux <soltanicialina777@gmail.com>
2025-06-28 18:33:18 +0300

    added classes from jakarta.validation.constraints

--- src/main/java/com/example/bankapp/dto/RegisterRequestDTO.java
+++ src/main/java/com/example/bankapp/dto/RegisterRequestDTO.java
@@ -1,5 +1,24 @@
 package com.example.bankapp.dto;
 
-public class RegisterRequestDTO {
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterRequestDTO {
+    @NotBlank(message = "Username is mandatory")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
+    private String username;
+
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Password is mandatory")
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String password;
 }
