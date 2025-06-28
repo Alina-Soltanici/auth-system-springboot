8dedb2d79356398b8d10e52100314cb36fb43d57
alinalinux <soltanicialina777@gmail.com>
2025-06-28 17:54:12 +0300

    Add RoleRepository interface for accessing Role data

--- src/main/java/com/example/bankapp/repository/RoleRepository.java
+++ src/main/java/com/example/bankapp/repository/RoleRepository.java
@@ -1,4 +1,10 @@
 package com.example.bankapp.repository;
 
-public interface RoleRepository {
+import com.example.bankapp.domain.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
 }
