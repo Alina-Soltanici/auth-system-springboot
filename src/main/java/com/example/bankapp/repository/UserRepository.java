0878c14ac3210e2d607beace08680b1b2fe51160
alinalinux <soltanicialina777@gmail.com>
2025-06-28 17:54:33 +0300

    Add UserRepository interface for managing User entities

--- src/main/java/com/example/bankapp/repository/UserRepository.java
+++ src/main/java/com/example/bankapp/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.example.bankapp.repository;
 
-public interface UserRepository {
+import com.example.bankapp.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
 }
